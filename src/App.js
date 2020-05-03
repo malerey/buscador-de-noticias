a1c14d52c5d527c7df19ab3a2a721e711c26c7d2
GuadaIt <guada.iturralde@gmail.com>
2020-05-02 21:19:06 -0300

    agrego app.js y app.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+import CardsContainer from './components/CardsContainer';
+import Footer from './components/Footer';
 
-function App() {
+const App = () => {
+
+  const [news, setNews] = useState([]);
+  
+  useEffect(() => {
+    fetch(`http://newsapi.org/v2/top-headlines?country=ar&apiKey=API_KEY`)
+      .then(res => res.json())
+      .then(data => {
+        setNews(data.articles);
+      })
+  }, []);
+
+  const filtroFuente = e => {
+    fetch(`http://newsapi.org/v2/top-headlines?sources=${e.target.id}&apiKey=API_KEY`)
+      .then(res => res.json())
+      .then(data => {
+        setNews(data.articles);
+      })  
+  };
+
+  const busqueda = searchInput => {
+    fetch(`http://newsapi.org/v2/everything?language=es&q=${searchInput}&apiKey=API_KEY`)
+      .then(res => res.json())
+      .then(data => {
+        setNews(data.articles);
+      }) 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav filtroFuente={filtroFuente} busqueda={busqueda}/>      
+      <CardsContainer newsArray={news}/>
+      <Footer/>      
+      
+      <Switch>
+       <Route exact path='./source/:sourceName' component= {() => <CardsContainer newsArray={news}/>}/>
+       <Route path='./search/:searchInput' component={() => <CardsContainer newsArray={news}/>}/>
+      </Switch>
+    </Router>
   );
 }
 
